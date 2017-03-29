ae95a3598887ecffbd57f4a9872953bd99e80b16
mayi6349 <may@MaydeMacBook-Pro.local>
2017-03-29 14:56:54 +0800

    表名追加

--- src/com/jfinal/plugin/activerecord/generator/DataDictionaryGenerator.java
+++ src/com/jfinal/plugin/activerecord/generator/DataDictionaryGenerator.java
@@ -26,6 +26,9 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 import javax.sql.DataSource;
+
+import org.apache.commons.lang.StringUtils;
+
 import com.jfinal.kit.LogKit;
 import com.jfinal.kit.StrKit;
 
@@ -37,39 +40,39 @@ public class DataDictionaryGenerator {
 	protected DataSource dataSource;
 	protected String dataDictionaryOutputDir;
 	protected String dataDictionaryFileName = "_DataDictionary.txt";
-	
+
 	public DataDictionaryGenerator(DataSource dataSource, String dataDictionaryOutputDir) {
 		this.dataSource = dataSource;
 		this.dataDictionaryOutputDir = dataDictionaryOutputDir;
 	}
-	
+
 	public void setDataDictionaryOutputDir(String dataDictionaryOutputDir) {
 		if (StrKit.notBlank(dataDictionaryOutputDir))
 			this.dataDictionaryOutputDir = dataDictionaryOutputDir;
 	}
-	
+
 	public void setDataDictionaryFileName(String dataDictionaryFileName) {
 		if (StrKit.notBlank(dataDictionaryFileName))
 			this.dataDictionaryFileName = dataDictionaryFileName;
 	}
-	
+
 	public void generate(List<TableMeta> tableMetas) {
 		System.out.println("Generate DataDictionary file ...");
 		rebuildColumnMetas(tableMetas);
-		
+
 		StringBuilder ret = new StringBuilder();
 		for (TableMeta tableMeta : tableMetas)
 			generateTable(tableMeta, ret);
-		
+
 		wirtToFile(ret);
 	}
-	
+
 	protected void generateTable(TableMeta tableMeta, StringBuilder ret) {
 		ret.append("Table: ").append(tableMeta.name);
 		if (StrKit.notBlank(tableMeta.remarks))
 			ret.append("\tRemarks: ").append(tableMeta.remarks);
 		ret.append("\n");
-		
+
 		String sparateLine = genSeparateLine(tableMeta);
 		ret.append(sparateLine);
 		genTableHead(tableMeta, ret);
@@ -79,24 +82,25 @@ public class DataDictionaryGenerator {
 		ret.append(sparateLine);
 		ret.append("\n");
 	}
-	
+
 	/*
-	-----------+---------+------+-----+---------+----------------
-	 Field     | Type    | Null | Key | Default | Remarks
-	-----------+---------+------+-----+---------+----------------
-	 id		   | int(11) | NO	| PRI | NULL	| remarks here	
-	*/
-	protected void genCell(int columnMaxLen, String preChar, String value, String fillChar, String postChar, StringBuilder ret) {
+	 * -----------+---------+------+-----+---------+---------------- Field |
+	 * Type | Null | Key | Default | Remarks
+	 * -----------+---------+------+-----+---------+---------------- id |
+	 * int(11) | NO | PRI | NULL | remarks here
+	 */
+	protected void genCell(int columnMaxLen, String preChar, String value, String fillChar, String postChar,
+			StringBuilder ret) {
 		ret.append(preChar);
 		ret.append(value);
-		for (int i=0, n=columnMaxLen-value.length() + 1; i<n; i++)
-			ret.append(fillChar);	// 值后的填充字符，值为 " "、"-"
+		for (int i = 0, n = columnMaxLen - value.length() + 1; i < n; i++)
+			ret.append(fillChar); // 值后的填充字符，值为 " "、"-"
 		ret.append(postChar);
 	}
-	
+
 	protected String genSeparateLine(TableMeta tm) {
 		StringBuilder ret = new StringBuilder();
-		genCell(tm.colNameMaxLen, "-", "---", "-", "+",  ret);
+		genCell(tm.colNameMaxLen, "-", "---", "-", "+", ret);
 		genCell(tm.colTypeMaxLen, "-", "---", "-", "+", ret);
 		genCell("Null".length(), "-", "---", "-", "+", ret);
 		genCell("Key".length(), "-", "---", "-", "+", ret);
@@ -105,19 +109,19 @@ public class DataDictionaryGenerator {
 		ret.append("\n");
 		return ret.toString();
 	}
-	
+
 	protected void genTableHead(TableMeta tm, StringBuilder ret) {
-		genCell(tm.colNameMaxLen, " ", "Field", " ", "|",  ret);
+		genCell(tm.colNameMaxLen, " ", "Field", " ", "|", ret);
 		genCell(tm.colTypeMaxLen, " ", "Type", " ", "|", ret);
-		genCell("Null".length(), " ", "Null",  " ","|", ret);
-		genCell("Key".length(), " ", "Key",  " ","|", ret);
+		genCell("Null".length(), " ", "Null", " ", "|", ret);
+		genCell("Key".length(), " ", "Key", " ", "|", ret);
 		genCell(tm.colDefaultValueMaxLen, " ", "Default", " ", "|", ret);
 		genCell("Remarks".length(), " ", "Remarks", " ", "", ret);
 		ret.append("\n");
 	}
-	
+
 	protected void genColumn(TableMeta tableMeta, ColumnMeta columnMeta, StringBuilder ret) {
-		genCell(tableMeta.colNameMaxLen, " ", columnMeta.name, " ", "|",  ret);
+		genCell(tableMeta.colNameMaxLen, " ", columnMeta.name, " ", "|", ret);
 		genCell(tableMeta.colTypeMaxLen, " ", columnMeta.type, " ", "|", ret);
 		genCell("Null".length(), " ", columnMeta.isNullable, " ", "|", ret);
 		genCell("Key".length(), " ", columnMeta.isPrimaryKey, " ", "|", ret);
@@ -125,7 +129,7 @@ public class DataDictionaryGenerator {
 		genCell("Remarks".length(), " ", columnMeta.remarks, " ", "", ret);
 		ret.append("\n");
 	}
-	
+
 	protected void rebuildColumnMetas(List<TableMeta> tableMetas) {
 		Connection conn = null;
 		try {
@@ -137,26 +141,28 @@ public class DataDictionaryGenerator {
 				ResultSet rs = dbMeta.getColumns(conn.getCatalog(), null, tableMeta.name, null);
 				while (rs.next()) {
 					ColumnMeta columnMeta = new ColumnMeta();
-					columnMeta.name = rs.getString("COLUMN_NAME");			// 名称
-					
-					columnMeta.type = rs.getString("TYPE_NAME");			// 类型
+					columnMeta.name = rs.getString("COLUMN_NAME"); // 名称
+
+					columnMeta.type = rs.getString("TYPE_NAME"); // 类型
 					if (columnMeta.type == null)
 						columnMeta.type = "";
-					
-					int columnSize = rs.getInt("COLUMN_SIZE");				// 长度
+
+					int columnSize = rs.getInt("COLUMN_SIZE"); // 长度
 					if (columnSize > 0) {
 						columnMeta.type = columnMeta.type + "(" + columnSize;
-						int decimalDigits = rs.getInt("DECIMAL_DIGITS");	// 小数位数
+						int decimalDigits = rs.getInt("DECIMAL_DIGITS"); // 小数位数
 						if (decimalDigits > 0) {
 							columnMeta.type = columnMeta.type + "," + decimalDigits;
 						}
 						columnMeta.type = columnMeta.type + ")";
 					}
-					
-					columnMeta.isNullable = rs.getString("IS_NULLABLE");	// 是否允许 NULL 值
+
+					columnMeta.isNullable = rs.getString("IS_NULLABLE"); // 是否允许
+																			// NULL
+																			// 值
 					if (columnMeta.isNullable == null)
 						columnMeta.isNullable = "";
-					
+
 					columnMeta.isPrimaryKey = "   ";
 					String[] keys = tableMeta.primaryKey.split(",");
 					for (String key : keys) {
@@ -165,36 +171,46 @@ public class DataDictionaryGenerator {
 							break;
 						}
 					}
-					
-					columnMeta.defaultValue = rs.getString("COLUMN_DEF");	// 默认值
+
+					columnMeta.defaultValue = rs.getString("COLUMN_DEF"); // 默认值
 					if (columnMeta.defaultValue == null)
 						columnMeta.defaultValue = "";
-					
-					columnMeta.remarks = rs.getString("REMARKS");			// 备注
+
+					columnMeta.remarks = rs.getString("REMARKS"); // 备注
+					// 表名 May
+					if (StringUtils.isEmpty(tableMeta.remarks)) {
+						String tableName = rs.getString("REMARKS");
+						if (tableName.contains("ID") || tableName.contains("Id")) {
+							tableMeta.remarks = tableName.replace("ID", "表").replace("Id", "表");
+						}
+					}
+
 					if (columnMeta.remarks == null)
 						columnMeta.remarks = "";
-					
+
 					if (tableMeta.colNameMaxLen < columnMeta.name.length())
 						tableMeta.colNameMaxLen = columnMeta.name.length();
 					if (tableMeta.colTypeMaxLen < columnMeta.type.length())
 						tableMeta.colTypeMaxLen = columnMeta.type.length();
 					if (tableMeta.colDefaultValueMaxLen < columnMeta.defaultValue.length())
 						tableMeta.colDefaultValueMaxLen = columnMeta.defaultValue.length();
-					
+
 					tableMeta.columnMetas.add(columnMeta);
 				}
 				rs.close();
 			}
-		}
-		catch (SQLException e) {
+		} catch (SQLException e) {
 			throw new RuntimeException(e);
-		}
-		finally {
+		} finally {
 			if (conn != null)
-				try {conn.close();} catch (SQLException e) {LogKit.error(e.getMessage(), e);}
+				try {
+					conn.close();
+				} catch (SQLException e) {
+					LogKit.error(e.getMessage(), e);
+				}
 		}
 	}
-	
+
 	/**
 	 * _DataDictionary.txt 覆盖写入
 	 */
@@ -204,17 +220,19 @@ public class DataDictionaryGenerator {
 			File dir = new File(dataDictionaryOutputDir);
 			if (!dir.exists())
 				dir.mkdirs();
-			
+
 			String target = dataDictionaryOutputDir + File.separator + dataDictionaryFileName;
 			fw = new FileWriter(target);
 			fw.write(ret.toString());
-		}
-		catch (IOException e) {
+		} catch (IOException e) {
 			throw new RuntimeException(e);
-		}
-		finally {
+		} finally {
 			if (fw != null)
-				try {fw.close();} catch (IOException e) {LogKit.error(e.getMessage(), e);}
+				try {
+					fw.close();
+				} catch (IOException e) {
+					LogKit.error(e.getMessage(), e);
+				}
 		}
 	}
 }
